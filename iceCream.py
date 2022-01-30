2ff850c858ceb281cc13c2dbb353d2d33bc5432e
Bhoomi Singh <ymailtobhoomi@gmail.com>
2022-01-30 13:41:26 +0530

    Update iceCream.py

--- iceCream.py
+++ iceCream.py
@@ -1,23 +1,33 @@
-import csv
 import plotly.express as px
+import csv
 import numpy as np
 
-def getDataSource(data_Path):
-    iceCream_sales=[]
-    coldDrink_sales=[]
-    with open("IceCream.csv") as f:
-        df = csv.DictReader(f)
-        for row in df:
-            iceCream_sales.append(float(row['Temperature']))
-            coldDrink_sales.append(float(row['Ice-cream Sales( â‚¹ )']))
-    return{"x: iceCreame_sales", "y: coldDrink_sales"}
+def plotFigure(data_path):
+    with open(data_path) as csv_file:
+        df = csv.DictReader(csv_file)
+        fig = px.scatter(df,x="Temperature", y="Ice-cream")
+        fig.show()
 
-def findCorrelation(data_source):
-    correlation= np.corrcoef(data_source["x"],data_source["y"])
-    print("Corelation between Ice-Cream and Cold Drink", correlation[0,1])
+def getDataSource(data_path):
+    ice_cream_sales = []
+    cold_drink_sales = []
+    with open(data_path) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            ice_cream_sales.append(float(row["Temperature"]))
+            cold_drink_sales.append(float(row["Ice-cream"]))
+
+    return {"x" : ice_cream_sales, "y": cold_drink_sales}
+
+def findCorrelation(datasource):
+    correlation = np.corrcoef(datasource["x"], datasource["y"])
+    print("Correlation between Temperature vs Ice Cream Sales :-  \n--->",correlation[0,1])
 
 def setup():
-    data_Path="IceCream.csv"
-    data_source=getDataSource(data_Path)
-    findCorrelation(data_source)
+    data_path  = "ice.csv"
+
+    datasource = getDataSource(data_path)
+    findCorrelation(datasource)
+    plotFigure(data_path)
+
 setup()
